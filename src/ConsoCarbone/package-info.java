110db9fb9f48f56cd2404c77394a50ef9ccd4634
Jenna Journo <jennajourno02@gmail.com>
2022-12-20 12:06:59 +0100

    javadoc package

---
+++ src/ConsoCarbone/package-info.java
@@ -0,0 +1,4 @@
+/** ConsoCarbone contient l'ensemble des postes de consommation concernant l'alimentation, le logement, les transports,
+ * les depenses en biens de consommation et les services publics
+ */
+package ConsoCarbone;
---
+++ src/Utilisateur/package-info.java
@@ -0,0 +1,3 @@
+/** Utilisateur permet de gerer la consommation carbone d'un utilisateur mais aussi d'un ensemble d'utilisateur formant une population
+ */
+package Utilisateur;
